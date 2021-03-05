f4b0d019fc3c61bde2a0a869154247555d8f97df
Vicente Canales <vicente.canales@automattic.com>
2021-03-05 16:09:02 -0300

    remove script registration from nav block

--- packages/block-library/src/navigation/index.php
+++ packages/block-library/src/navigation/index.php
@@ -121,22 +121,6 @@ function render_block_core_navigation( $attributes, $content, $block ) {
 
 	unset( $attributes['rgbTextColor'], $attributes['rgbBackgroundColor'] );
 
-	// wp_enqueue_script(
-	// 'core_block_navigation_load_frontend_scripts',
-	// plugins_url( 'gutenberg/build/block-library-frontend/index.js' ),
-	// array(),
-	// null,
-	// true
-	// );
-
-	wp_register_script(
-		'core_block_navigation_load_frontend_scripts',
-		plugins_url( 'gutenberg/build/block-library-frontend/index.js' ),
-		array(),
-		false,
-		true
-	);
-
 	if ( empty( $block->inner_blocks ) ) {
 		return '';
 	}
